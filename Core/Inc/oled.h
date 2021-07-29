195caef0c4df8000e2d88db539e92f88df570878
xsine <neoway@126.com>
2021-07-29 16:08:45 +0800

    init

--- Core/Inc/oled.h
+++ Core/Inc/oled.h
@@ -4,8 +4,6 @@
 #include "main.h"
 #include "u8g2.h"
 
-
-
 #define OLED_ADDRESS 0x78
 uint8_t u8x8_byte_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 extern uint8_t u8x8_gpio_and_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
